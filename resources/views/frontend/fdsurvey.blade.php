7b3a14763e7cb0d9543d2990ffe1e45e3e3d93a7
Hani G. <websiteadmin@naegypt.org>
2025-11-16 21:38:13 +0200

    FIX: Changed endpoint

--- resources/views/frontend/fdsurvey.blade.php
+++ resources/views/frontend/fdsurvey.blade.php
@@ -111,7 +111,7 @@
         </div>
     </header>
 
-    <form id="educationSurveyForm" action="https://form.taxi/s/noaxqol0" method="POST">
+    <form id="educationSurveyForm" action="https://formcarry.com/s/NiRt3kihplw" method="POST">
         <input type="hidden" name="_gotcha" style="display:none !important">
         <div class="section">
             <h2> بيانات المشارك</h2>
