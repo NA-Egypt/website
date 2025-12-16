ada0a155f0caea717bd5d55768a5fc7fbebc01bd
Hani G. <websiteadmin@naegypt.org>
2025-12-16 22:33:11 +0200

    Dashboard adjustments

--- resources/views/components/dashboard/card-statics.blade.php
+++ resources/views/components/dashboard/card-statics.blade.php
@@ -1,18 +1,14 @@
-{{-- First 4 cards in dashboard --}}
-
 @props(['name', 'qty', 'icon'])
-<div class="col-12 col-lg-6 col-xl-6 d-flex">
-    <div class="card radius-10 ">
-      <div class="card-body custom-shadow">
-          <div class="d-flex align-items-center ">
-              <div>
-                  <p class="mb-0 text-secondary">{{ $name }}</p>
-                  <h4 class="my-1">{{ $qty }}</h4>
-              </div>
-              <div {{ $attributes->merge(["class"=>"widget-icon-large text-white ms-auto"]) }} >
-                <i class="bi bi-{{ $icon }}"></i>
-              </div>
+<div class="card radius-10 w-100">
+  <div class="card-body custom-shadow">
+      <div class="d-flex align-items-center ">
+          <div>
+              <p class="mb-0 text-secondary">{{ $name }}</p>
+              <h4 class="my-1">{{ $qty }}</h4>
+          </div>
+          <div {{ $attributes->merge(["class"=>"widget-icon-large text-white ms-auto"]) }} >
+            <i class="bi bi-{{ $icon }}"></i>
           </div>
       </div>
-    </div>
   </div>
+</div>
--- resources/views/dashborad.blade.php
+++ resources/views/dashborad.blade.php
@@ -4,52 +4,48 @@
 
     
     {{-- Cards --}}
-    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-2 row-cols-xl-4">
+    {{-- Cards --}}
+    <div class="row row-cols-1 row-cols-md-2 row-cols-xl-4 g-3 mb-4">
       
       {{-- total meetings --}}
-      <a href="{{ route('meeting.index') }}">
-        <x-dashboard.card-statics name="{{ __('messages.Total') . ' ' . __('messages.Meetings') }}" qty="{{ $meetings->count() }}" class="bg-gradient-info " icon="calendar-week" />
-      </a>
+      <div class="col">
+        <a href="{{ route('meeting.index') }}" class="text-decoration-none">
+          <x-dashboard.card-statics name="{{ __('messages.Total') . ' ' . __('messages.Meetings') }}" qty="{{ $meetings->count() }}" class="bg-gradient-info " icon="calendar-week" />
+        </a>
+      </div>
       {{-- / total meetings --}}
     
       {{-- total Groups --}}
-      <a href="{{ route('group.index') }}">
-        <x-dashboard.card-statics name="{{ __('messages.Total') . ' ' . __('messages.Groups') }}" qty="{{ $groups->count() }}" class="bg-gradient-info " icon="house" />
-      </a>
+      <div class="col">
+        <a href="{{ route('group.index') }}" class="text-decoration-none">
+          <x-dashboard.card-statics name="{{ __('messages.Total') . ' ' . __('messages.Groups') }}" qty="{{ $groups->count() }}" class="bg-gradient-info " icon="house" />
+        </a>
+      </div>
       {{-- / total Groups --}}
 
       {{-- Total cities --}}
-      <a href="{{ route('city.index') }}">
-        <x-dashboard.card-statics name="{{ __('messages.Total') . ' ' . __('messages.Cities') }}" qty="{{ $cities->count() }}" class="bg-gradient-info " icon="globe" />
-      </a>
+      <div class="col">
+        <a href="{{ route('city.index') }}" class="text-decoration-none">
+          <x-dashboard.card-statics name="{{ __('messages.Total') . ' ' . __('messages.Cities') }}" qty="{{ $cities->count() }}" class="bg-gradient-info " icon="globe" />
+        </a>
+      </div>
       {{-- / Total cities --}}
 
       {{-- Total Users --}}
-      <div class="col-12 col-lg-12 col-xl-12 d-flex">
-        <div class="card radius-10">
-          <div class="card-body">
-              <div class="d-flex align-items-center">
-                  <div>
-                      <p class="mb-0 text-secondary">{{__('messages.Total Users')}}</p>
-                      <h4 class="my-1">5</h4>
-                  </div>
-                  <div class="widget-icon-large bg-gradient-info text-white ms-auto"><i class="bi bi-people"></i>
-                  </div>
-              </div>
-          </div>
-        </div>
+      <div class="col">
+          <x-dashboard.card-statics name="{{ __('messages.Total Users') }}" qty="5" class="bg-gradient-info " icon="people" />
       </div>
       {{-- / Total Users --}}
     </div>
     {{-- / Cards --}}
 
-    <div class="row">
+    <div class="row g-3 mb-4">
 
       {{-- Groups --}}
-      <div class="col-3 col-lg-3 col-xl-3 d-flex">
+      <div class="col-12 col-lg-4 d-flex">
         <div class="card radius-10 w-100">
           <x-dashboard.card-header>{{ __('messages.Groups') }}</x-dashboard.card-header>
-          <div class="d-flex justify-content-end">
+          <div class="d-flex justify-content-end px-3">
             <input type="search" id="search-input" class="form-control form-control-sm" placeholder="Search">
           </div>
           <div class="top-sellers-list p-2 mb-3">
@@ -60,7 +56,7 @@
       {{-- Groups --}}
 
       {{-- List Of Meetings in Spacific City --}}
-      <div class="col-3 col-lg-3 col-xl-3 d-flex">
+      <div class="col-12 col-lg-4 d-flex">
         <div class="card radius-10 w-100">
           <x-dashboard.card-header>{{ __('messages.Meetings') . ' ' . __('messages.in') . ' ' . __('messages.City') }}</x-dashboard.card-header>
           <div class="card-body">
@@ -77,7 +73,7 @@
 
 
       {{-- List Of Groups in Spacific City --}}
-      <div class="col-3 col-lg-3 col-xl-3 d-flex">
+      <div class="col-12 col-lg-4 d-flex">
         <div class="card radius-10 w-100">
           <x-dashboard.card-header>{{ __('messages.Groups') . ' ' . __('messages.in') . ' ' . __('messages.City') }}</x-dashboard.card-header>
           <div class="card-body">
@@ -94,49 +90,53 @@
     </div>
   
     {{-- Recent Transactions --}}
-    <div class="card radius-10 w-50" dir="ltr">
-      <div class="card-header bg-secondary bg-gradient">
-        <div class="row g-3 align-items-center ">
-          <div class="col-12 col-lg-6 col-xl-6 d-flex" dir="ltr">
-            <p class="mb-0 text-black">{{ __('messages.Recent Logs')}}</p>
+    <div class="row">
+      <div class="col-12">
+        <div class="card radius-10 w-100" dir="ltr">
+          <div class="card-header bg-secondary bg-gradient">
+            <div class="row g-3 align-items-center ">
+              <div class="col-12 col-lg-6 col-xl-6 d-flex" dir="ltr">
+                <p class="mb-0 text-black">{{ __('messages.Recent Logs')}}</p>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div class="card-body">
-        <div class="table-responsive w-100">
-          <table class="table align-middle mb-0" dir="ltr">
-            <thead class="table-dark">
-              <tr>
-                {{-- <th>#{{ __('messages.ID')}}</th> --}}
-                <th>{{  __('messages.Operation') }}</th>
-                <th>{{  __('messages.Model') }}</th>
-                <th>{{  __('messages.User') }}</th>
-                <th>{{  __('messages.Date') }}</th>
-                <th>{{  __('messages.Time') }}</th>
-                <th>{{  __('messages.Email') }}</th>
-              </tr>
-            </thead>
-            <tbody>
-              @foreach ($transactions as $trans)                    
-                <tr>
-                  {{-- <td>{{ $trans->id }}</td> --}}
-                  <td>{{ ucfirst($trans->operation) }}</td>
-                  <td>{{ $trans->model }}</td>
-                  <td>{{ $trans->user->name ?? 'System' }}</td>
-                  <td>{{ $trans->created_at->format('Y-m-d') }}</td>
-                  <td>{{ $trans->created_at->format('H:i:s') }}</td>
-                  <td>
-                      @if ($trans->model === 'Meeting')
-                          {{ $trans->user->email }}
+          <div class="card-body">
+            <div class="table-responsive w-100">
+              <table class="table align-middle mb-0" dir="ltr">
+                <thead class="table-dark">
+                  <tr>
+                    {{-- <th>#{{ __('messages.ID')}}</th> --}}
+                    <th>{{  __('messages.Operation') }}</th>
+                    <th>{{  __('messages.Model') }}</th>
+                    <th>{{  __('messages.User') }}</th>
+                    <th>{{  __('messages.Date') }}</th>
+                    <th>{{  __('messages.Time') }}</th>
+                    <th>{{  __('messages.Email') }}</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach ($transactions as $trans)                    
+                    <tr>
+                      {{-- <td>{{ $trans->id }}</td> --}}
+                      <td>{{ ucfirst($trans->operation) }}</td>
+                      <td>{{ $trans->model }}</td>
+                      <td>{{ $trans->user->name ?? 'System' }}</td>
+                      <td>{{ $trans->created_at->format('Y-m-d') }}</td>
+                      <td>{{ $trans->created_at->format('H:i:s') }}</td>
+                      <td>
+                          @if ($trans->model === 'Meeting')
+                              {{ $trans->user->email }}
 
-                      @else
-                      {{  $trans->user->email ?? $trans->details['en_name'] }}
-                      @endif
-                  </td>
-                </tr>
-              @endforeach
-            </tbody>
-          </table>
+                          @else
+                          {{  $trans->user->email ?? $trans->details['en_name'] }}
+                          @endif
+                      </td>
+                    </tr>
+                  @endforeach
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
