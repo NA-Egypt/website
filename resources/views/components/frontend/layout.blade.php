2561dbe65eef5d41b618e8d92e4ac6c872e72f91
Hani G. <websiteadmin@naegypt.org>
2026-07-17 07:52:22 +0300

    feat: add Google Analytics tracking tag to frontend and dashboard layouts

--- resources/views/components/frontend/layout.blade.php
+++ resources/views/components/frontend/layout.blade.php
@@ -42,6 +42,15 @@
 }
 </style>
 <title>{{__('messages.NA')}}</title>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TX958298Y6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TX958298Y6');
+</script>
 </head>
   <body class="hanken-grotesk {{$direction}}">
     <x-frontend.nav-bar />
