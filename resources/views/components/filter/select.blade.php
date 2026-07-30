374e9978457cc65e3530acc9274e13a1a7a593f7
HeronAgent <heronagent@heronsignal.com>
2026-07-30 11:38:12 +0000

    Fix meeting filter select clearing

--- resources/views/components/filter/select.blade.php
+++ resources/views/components/filter/select.blade.php
@@ -40,7 +40,7 @@
                     theme: 'bootstrap4',
                     width: '100%',
                     placeholder: '{{ __('messages.Choose') }} {{ $label }}...',
-                    allowClear: {{ $attributes->has('data-allow-clear') ? 'true' : 'false' }},
+                    allowClear: true,
                     dir: '{{ app()->getLocale() === "ar" ? "rtl" : "ltr" }}'
                 });
 
